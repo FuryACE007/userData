a7aee8ea25d74c863937f72ae3dce6b1c02c5fd8
Sudhanshu Shekhar <sudhanshuvshekhar@gmail.com>
2021-10-23 22:45:52 +0530

    added UsersInfo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import UserForm from "./components/UserForm";
+import UsersInfo from "./components/UsersInfo";
 
 function App() {
   return (
     <div className=" bg-black min-h-screen">
       <p className=" text-lg">Hello</p>
       <UserForm />
+      <UsersInfo />
     </div>
   );
 };
---
+++ src/components/UsersInfo.js
@@ -0,0 +1,11 @@
+const UsersInfo = () =>{
+    return(
+        <div className="container flex flex-col w-1/2 mx-auto bg-white rounded-md p-5 mt-6">
+            <div className ="container flex flex-col p-3">
+                Hi I am john!
+            </div>
+        </div>
+    );
+};
+
+export default UsersInfo;
